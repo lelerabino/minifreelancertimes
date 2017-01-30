f774f5d175d014ebf58aaf7bf917e748937e8ed1
lelerabino <lelerabino@gmail.com>
2017-01-30 17:24:36 +0100

    scaffolding "all" jasmine specRunner page to bundle all the specs in a single report

---
+++ public/tests/specs/app/modules/all/index.js
@@ -0,0 +1,7 @@
+specs = [
+    'tests/specs/app/modules/ApplicationSkeleton/module'
+    , 'tests/specs/app/modules/ApplicationSkeleton/layout'
+    , 'tests/specs/app/modules/timelogs/module'
+    , 'tests/specs/app/modules/weeklytimelogs/module'
+    , 'tests/specs/app/modules/weeklytimelogs/models'
+];
